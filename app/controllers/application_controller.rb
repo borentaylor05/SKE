bb01407727fbeceb42df7abe2af08956f5713fa6
Taylor Boren <taylorboren@teletech.com>
2015-02-02 15:58:12 -0700

    Added auth and more upload capability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -70,7 +70,7 @@ class ApplicationController < ActionController::Base
   def verify
     Rails.logger.info("ORIGIN -- #{request.original_url}")
     Rails.logger.info("URL SHOULD BE ----> #{request.original_url.gsub!(/token(=[^&]*)?|^token(=[^&]*)/, '')}")
-    if !$whitelist.include?(request.headers['origin']) && !token_valid(params[:token])
+    if (request.headers['origin'].nil? or !$whitelist.include?(request.headers['origin'])) and !token_valid(params[:token])
       Rails.logger.info("URL SHOULD BE ----> #{request.original_url.gsub!(/token(=[^&]*)?|^token(=[^&]*)/, '')}")
       cookies[:url] = request.original_url.gsub!(/token(=[^&]*)?|^token(=[^&]*)/, '')
       cookies[:action] = params[:action]
@@ -84,6 +84,7 @@ class ApplicationController < ActionController::Base
         end
       end
     else
+      Rails.logger.info("WRRRRRROOOOOOOOONG")
       return true
     end
   end
