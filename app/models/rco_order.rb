57cd29de107167febf374a87a86d908e42087500
Taylor Boren <taylorboren@teletech.com>
2015-06-11 12:28:31 -0600

    Fixing RCO

--- app/models/rco_order.rb
+++ app/models/rco_order.rb
@@ -1,9 +1,6 @@
 class RcoOrder < ActiveRecord::Base
 
 	validates :order_id, presence: true
-	validates :lms_num, presence: true
-	validates :coupon, presence: true
-	validates :num_registrations, presence: true
 	validates :agent_name, presence: true
 
 	default_scope { order('updated_at DESC') }
