8f174f866ab65f24d2a857662984946cfcafa59a
Taylor Boren <taylorboren@teletech.com>
2015-06-16 08:22:42 -0600

    Fixing RCO

--- app/controllers/arc_controller.rb
+++ app/controllers/arc_controller.rb
@@ -90,30 +90,25 @@ class ArcController < ApplicationController
 		if(request.method == "OPTIONS")
 			respond({status: 0})
 		elsif request.method == "POST"
-			rco = RcoOrder.find_by(order_id: params[:order_id])
-			if rco 
-				respond({ status: 1, error: "RCO with that Order number already exists." })
+			rco = RcoOrder.new(
+				order_id: params[:order_id],
+				agent_name: params[:agent_name],
+				created: params[:created] ? true : false,
+				acct_with_password: params[:acct_with_password] ? true : false,
+				acct_forgot_password: params[:acct_forgot_password] ? true : false,
+				no_share: params[:no_share] ? true : false,
+				order_id: params[:order_id],
+				lms_num: params[:lms_num],
+				num_registrations: params[:num_registrations],
+				coupon: params[:coupon],
+				paypal: params[:paypal] ? true : false,					
+				comments: params[:comments]
+			)
+			if rco.valid?
+				rco.save
+				respond({ status: 0, rco: rco })
 			else
-				rco = RcoOrder.new(
-					order_id: params[:order_id],
-					agent_name: params[:agent_name],
-					created: params[:created] ? true : false,
-					acct_with_password: params[:acct_with_password] ? true : false,
-					acct_forgot_password: params[:acct_forgot_password] ? true : false,
-					no_share: params[:no_share] ? true : false,
-					order_id: params[:order_id],
-					lms_num: params[:lms_num],
-					num_registrations: params[:num_registrations],
-					coupon: params[:coupon],
-					paypal: params[:paypal] ? true : false,					
-					comments: params[:comments]
-				)
-				if rco.valid?
-					rco.save
-					respond({ status: 0, rco: rco })
-				else
-					respond({ status: 1, error: rco.errors.full_messages })
-				end
+				respond({ status: 1, error: rco.errors.full_messages })
 			end
 		end
 	end
