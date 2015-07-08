cbab460ffaf14f73e92bc5cbce9e9f451611f84f
Taylor Boren <taylorboren@teletech.com>
2015-07-08 14:11:43 -0600

    Added limit to people search

--- app/models/arc_check_tracker.rb
+++ app/models/arc_check_tracker.rb
@@ -1,14 +1,14 @@
 class ArcCheckTracker < ActiveRecord::Base
 
-	validates :check_num, presence: true
-	validates :agent_name, presence: true
-	validates :check_date, presence: true
-	validates :org, presence: true
-	validates :check_amount, presence: true
-	validates :case_id, presence: true
-	validates :check_name, presence: true
-	validates :state, presence: true
-	validates :tsc_received, presence: true
+	# validates :check_num, presence: true
+	# validates :agent_name, presence: true
+	# validates :check_date, presence: true
+	# validates :org, presence: true
+	# validates :check_amount, presence: true
+	# validates :case_id, presence: true
+	# validates :check_name, presence: true
+	# validates :state, presence: true
+	# validates :tsc_received, presence: true
 
 	default_scope { order('updated_at DESC') }
 	scope :search_string, -> (search) { where("lower(agent_name) like ? or lower(org) like ? or lower(check_name) like ? or lower(order_num) like ? or lower(crs) like ?", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%")}
