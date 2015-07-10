ba7684b3a55ccf14c0fe48c4b8041df8608c3888
Taylor Boren <taylorboren@teletech.com>
2015-07-10 13:36:30 -0600

    fixed search

--- app/models/arc_check_tracker.rb
+++ app/models/arc_check_tracker.rb
@@ -12,6 +12,6 @@ class ArcCheckTracker < ActiveRecord::Base
 
 	default_scope { order('updated_at DESC') }
 	scope :search_string, -> (search) { where("lower(agent_name) like ? or lower(org) like ? or lower(check_name) like ? or lower(order_num) like ? or lower(crs) like ?", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%")}
-	scope :search_numeric, -> (search) { where("cast(check_num as varchar) like ? or cast(amount as varchar) like ? or cast(case_id as varchar) like ? or order_num like ? or crs like ?", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%" )}
+	scope :search_numeric, -> (search) { where("cast(check_num as varchar) like ? or cast(check_amount as varchar) like ? or cast(case_id as varchar) like ? or order_num like ? or crs like ?", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%" )}
 
 end
