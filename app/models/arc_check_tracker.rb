7ef196887c93f7e99111095f9ab4c61aa50e23a2
Taylor Boren <taylorboren@teletech.com>
2015-07-10 13:34:31 -0600

    fixed search

--- app/models/arc_check_tracker.rb
+++ app/models/arc_check_tracker.rb
@@ -5,13 +5,13 @@ class ArcCheckTracker < ActiveRecord::Base
 	validates :check_date, presence: true
 	validates :org, presence: true
 	validates :check_amount, presence: true
-#	validates :case_id, presence: true
+	validates :case_id, presence: true
 	validates :check_name, presence: true
 	validates :state, presence: true
 	validates :tsc_received, presence: true
 
 	default_scope { order('updated_at DESC') }
 	scope :search_string, -> (search) { where("lower(agent_name) like ? or lower(org) like ? or lower(check_name) like ? or lower(order_num) like ? or lower(crs) like ?", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%")}
-	scope :search_numeric, -> (search) { where("cast(check_num as varchar) like ? or cast(case_id as varchar) like ? or order_num like ? or crs like ? or check_amount like ?", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%" )}
+	scope :search_numeric, -> (search) { where("cast(check_num as varchar) like ? or cast(amount as varchar) like ? or cast(case_id as varchar) like ? or order_num like ? or crs like ?", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%", "%#{search.to_s}%" )}
 
 end
