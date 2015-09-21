104d350f9c27ca7ee1cde6c61540fd290a73155f
Taylor Boren <taylorboren@teletech.com>
2015-09-21 10:51:19 -0600

    AB import now destroys all before uploading new info

--- lib/CDC.rb
+++ lib/CDC.rb
@@ -14,7 +14,6 @@ class CDC
 		CSV.foreach(file, headers: true) do |row|
 			if row[0]
 				row[0] = row[0].strip if row[0] # row[0] is Program Description
-				row = row.map { |col| col = "-" if col.nil? }
 				logger.info row
 				entry = AddressBookEntry.find_by(ProgramDescription: row[0])
 				if entry
